d9c3933758f86c4a50e36720b7bcbbd3006b2ad4
Hazdard <sacha.ben-arous@ens-paris-saclay.fr>
2023-04-18 11:56:05 +0200

    alpha_break function

--- src/arrays_method.ml
+++ src/arrays_method.ml
@@ -9,16 +9,14 @@ type tree_formula =
   | TForall of int * tree_formula
   | TExists of int * tree_formula
 
-type tree =
-  | Nil
-  | Node of tree_formula * bool * tree * tree
+type tree = Nil | Node of tree_formula * bool * tree * tree
 
 let rec tree_formula_of_formula form =
   let open Logic_formulas in
   match form with
   | True -> TTrue
   | False -> TFalse
-  | Not f-> TNot (tree_formula_of_formula f)
+  | Not f -> TNot (tree_formula_of_formula f)
   | Var i -> TVar i
   | And (f1, f2) -> TAnd (tree_formula_of_formula f1, tree_formula_of_formula f2)
   | Or (f1, f2) -> TOr (tree_formula_of_formula f1, tree_formula_of_formula f2)
@@ -26,53 +24,72 @@ let rec tree_formula_of_formula form =
   | Exists (i, f) -> TExists (i, tree_formula_of_formula f)
 
 let rec tree_of_formula_list l =
-  match l with 
+  match l with
   | [] -> Nil
-  | a::q -> Node(tree_formula_of_formula a, false, tree_of_formula_list q,Nil)    
+  | a :: q ->
+      Node (tree_formula_of_formula a, false, tree_of_formula_list q, Nil)
 
-let rec search_alpha t = 
-  match t with  
+let rec search_alpha t =
+  match t with
   | [] -> failwith "No alpha term"
-  | TAnd(f1,f2)::q -> (TAnd(f1,f2),q)
-  | node::q -> let a,b = search_alpha q in (a,node::b)
+  | TAnd (f1, f2) :: q -> (TAnd (f1, f2), q)
+  | node :: q ->
+      let a, b = search_alpha q in
+      (a, node :: b)
 
-let rec search_delta l = 
-  match l with  
+let rec search_delta l =
+  match l with
   | [] -> failwith "No delta term"
-  | TExists(v,f)::q -> (TExists(v,f),q)
-  | node::q -> let a,b = search_delta q in (a,node::b)
+  | TExists (v, f) :: q -> (TExists (v, f), q)
+  | node :: q ->
+      let a, b = search_delta q in
+      (a, node :: b)
 
-let rec search_beta l = 
-  match l with  
+let rec search_beta l =
+  match l with
   | [] -> failwith "No beta term"
-  | TOr(f1,f2)::q -> (TOr(f1,f2),q)
-  | node::q -> let a,b = search_beta q in (a,node::b)
+  | TOr (f1, f2) :: q -> (TOr (f1, f2), q)
+  | node :: q ->
+      let a, b = search_beta q in
+      (a, node :: b)
 
-let rec search_gamma l = 
-  match l with  
+let rec search_gamma l =
+  match l with
   | [] -> failwith "No gamma term"
-  | TForall(v,f)::q -> (TForall(v,f),q)
-  | node::q -> let a,b = search_gamma q in (a,node::b)
+  | TForall (v, f) :: q -> (TForall (v, f), q)
+  | node :: q ->
+      let a, b = search_gamma q in
+      (a, node :: b)
 
-let rec leaf_append_one tree tapp = 
+let rec leaf_append_one tree tapp =
   match tree with
   | Nil -> tapp
-  | Node(form,b,Nil,Nil) -> Node(form,b,tapp,Nil)
-  | Node(form,b,t,Nil) -> Node(form,b,leaf_append_one t tapp,Nil)
-  | Node (form,b,Nil,t) -> Node(form,b,Nil,leaf_append_one t tapp)
-  | Node (form,b,t1,t2)-> Node(form,b,leaf_append_one t1 tapp,leaf_append_one t2 tapp)
+  | Node (form, b, Nil, Nil) -> Node (form, b, tapp, Nil)
+  | Node (form, b, t, Nil) -> Node (form, b, leaf_append_one t tapp, Nil)
+  | Node (form, b, Nil, t) -> Node (form, b, Nil, leaf_append_one t tapp)
+  | Node (form, b, t1, t2) ->
+      Node (form, b, leaf_append_one t1 tapp, leaf_append_one t2 tapp)
 
-let rec leaf_append_two tree tapp1 tapp2 = 
+let rec leaf_append_two tree tapp1 tapp2 =
   match tree with
   | Nil -> failwith "Empty tree"
-  | Node(form,b,Nil,Nil) -> Node(form,b,tapp1,tapp2)
-  | Node(form,b,t,Nil) -> Node(form,b,leaf_append_two t tapp1 tapp2,Nil)
-  | Node (form,b,Nil,t) -> Node(form,b,Nil,leaf_append_two t tapp1 tapp2)
-  | Node (form,b,t1,t2)-> Node(form,b,leaf_append_two t1 tapp1 tapp2,leaf_append_two t2 tapp1 tapp2)
-  
+  | Node (form, b, Nil, Nil) -> Node (form, b, tapp1, tapp2)
+  | Node (form, b, t, Nil) -> Node (form, b, leaf_append_two t tapp1 tapp2, Nil)
+  | Node (form, b, Nil, t) -> Node (form, b, Nil, leaf_append_two t tapp1 tapp2)
+  | Node (form, b, t1, t2) ->
+      Node
+        (form, b, leaf_append_two t1 tapp1 tapp2, leaf_append_two t2 tapp1 tapp2)
+
 let rec alpha_break tree =
   match tree with
-  |Nil -> (Nil,false)
-  |Node(f,true,t1,t2)-> let t12,b1 = alpha_break t1 in if b1 then (Node(f,true,t12,t2),b1) else let t22,b2=alpha_break t2 in (Node(f,true,t1,t22),b2)
-  |Node(TAnd(f1,f2),false,t1,t2) -> (Node(TAnd(f1,f2),true,leaf_append_one (leaf_append_one t1 (Node(f1,false,Nil,Nil))) (Node(f2,false,Nil,Nil)),t2),true)
-  |Node(f,false,t1,t2)-> let t12,b1 = alpha_break t1 in if b1 then (Node(f,true,t12,t2),b1) else let t22,b2=alpha_break t2 in (Node(f,true,t1,t22),b2)
+  | Nil -> (Nil, false)
+  | Node (TAnd (f1, f2), false, t1, t2) ->
+      let t = Node (TAnd (f1, f2), true, t1, t2) in
+      let n = Node (f1, false, Node (f2, false, Nil, Nil), Nil) in
+      (leaf_append_one t n, true)
+  | Node (f, b, t1, t2) ->
+      let t12, b1 = alpha_break t1 in
+      if b1 then (Node (f, b, t12, t2), b1)
+      else
+        let t22, b2 = alpha_break t2 in
+        (Node (f, b, t1, t22), b2)
