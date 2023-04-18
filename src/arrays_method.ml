5791973478bf6e188f202c825b53ab8dce57e43e
Hugo <frug.batt.dev@gmail.com>
2023-04-18 08:08:36 +0200

    firsts functions

--- src/arrays_method.ml
+++ src/arrays_method.ml
@@ -13,3 +13,19 @@ type tree =
   | Leaf of tree_formula
   | Unary of tree_formula * tree
   | Binary of tree_formula * tree * tree
+
+
+let rec tree_formula_of_formula form =
+  let open Logic_formulas in
+  match form with
+  | True -> TTrue
+  | False -> TFalse
+  | Var i -> TVar i
+  | And (f1, f2) -> TAnd (tree_formula_of_formula f1, tree_formula_of_formula f2)
+  | Or (f1, f2) -> TOr (tree_formula_of_formula f1, tree_formula_of_formula f2)
+  | Forall (i, f) -> TForall (i, tree_formula_of_formula f)
+  | Exists (i, f) -> TExists (i, tree_formula_of_formula f)
+
+let tree_of_formula_list l =
+  let mapped = List.map tree_formula_of_formula l in
+  assert false
