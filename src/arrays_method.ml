be07bf7e2db0ac018e7e769ee9c82710bdaa0cb2
Hugo <frug.batt.dev@gmail.com>
2023-04-18 11:43:49 +0200

    gamma break

--- src/arrays_method.ml
+++ src/arrays_method.ml
@@ -25,6 +25,24 @@ let rec tree_formula_of_formula form =
   | Forall (i, f) -> TForall (i, tree_formula_of_formula f)
   | Exists (i, f) -> TExists (i, tree_formula_of_formula f)
 
+
+let rec meta_formula_of_var_formula i = function
+  | TTrue -> TTrue
+  | TFalse -> TFalse
+  | TVar j -> 
+      if i = j then TMetaVar i
+      else TVar j
+  | TMetaVar j -> TMetaVar j
+  | TNot f -> TNot (meta_formula_of_var_formula i f)
+  | TAnd (f1, f2) -> TAnd (meta_formula_of_var_formula i f1, meta_formula_of_var_formula i f2)
+  | TOr (f1, f2) -> TOr (meta_formula_of_var_formula i f1, meta_formula_of_var_formula i f2)
+  | TForall (j, f) ->
+      if i = j then TForall (j, f)
+      else TForall (j, meta_formula_of_var_formula i f)
+  | TExists (j, f) ->
+      if i = j then TExists (j, f)
+      else TExists (j, meta_formula_of_var_formula i f)
+
 let tree_of_formula f = Node (f, false, Nil, Nil)
 
 let rec tree_of_formula_list l =
@@ -72,9 +90,14 @@ let rec beta_break = function
         let t2', b2 = beta_break t2 in
         (Node (f, b, t1, t2'), b2)
 
-let rec search_gamma l = 
-  match l with  
-  | [] -> failwith "No gamma term"
-  | TForall(v,f)::q -> (TForall(v,f),q)
-  | node::q -> let a,b = search_gamma q in (a,node::b)
-
+let rec gamma_break = function
+  | Nil -> (Nil, false)
+  | Node (TForall (i, f), false, t1, t2) ->
+      let t = Node (TForall (i, f), true, t1, t2) and n = Node (meta_formula_of_var_formula i f, false, Nil, Nil) in
+      (leaf_append_one t n, true)
+  | Node (f, b, t1, t2) ->
+      let t1', b1 = gamma_break t1 in
+      if b1 then (Node (f, b, t1', t2), true)
+      else
+        let t2', b2 = gamma_break t2 in
+        (Node (f, b, t1, t2'), b2)
