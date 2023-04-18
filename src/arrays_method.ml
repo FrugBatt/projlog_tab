8ee3a3149f6d5c4f39a7dff87264183b3de17734
Hazdard <sacha.ben-arous@ens-paris-saclay.fr>
2023-04-18 10:42:58 +0200

    append function

--- src/arrays_method.ml
+++ src/arrays_method.ml
@@ -21,6 +21,7 @@ let rec tree_formula_of_formula form =
   match form with
   | True -> TTrue
   | False -> TFalse
+  | Not f-> TNot (tree_formula_of_formula f)
   | Var i -> TVar i
   | And (f1, f2) -> TAnd (tree_formula_of_formula f1, tree_formula_of_formula f2)
   | Or (f1, f2) -> TOr (tree_formula_of_formula f1, tree_formula_of_formula f2)
@@ -43,16 +44,22 @@ let rec search_delta l =
   match l with  
   | [] -> failwith "No delta term"
   | TExists(v,f)::q -> (TExists(v,f),q)
-  | node::q -> let a,b = search_alpha q in (a,node::b)
+  | node::q -> let a,b = search_delta q in (a,node::b)
 
 let rec search_beta l = 
   match l with  
   | [] -> failwith "No beta term"
   | TOr(f1,f2)::q -> (TOr(f1,f2),q)
-  | node::q -> let a,b = search_alpha q in (a,node::b)
+  | node::q -> let a,b = search_beta q in (a,node::b)
 
 let rec search_gamma l = 
   match l with  
   | [] -> failwith "No gamma term"
   | TForall(v,f)::q -> (TForall(v,f),q)
-  | node::q -> let a,b = search_alpha q in (a,node::b)
+  | node::q -> let a,b = search_gamma q in (a,node::b)
+
+let rec leaf_append tree tapp = 
+  match tree with
+  | Nil -> tapp
+  | Unary(form,b,t) -> Unary(form,b,leaf_append t tapp)
+  | Binary(form,b,t1,t2) -> Binary(form,b,leaf_append t1 tapp,leaf_append t2 tapp)
