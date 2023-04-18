14555bc7158a0b6a26583adb5ed577ec7caa00db
Hugo <frug.batt.dev@gmail.com>
2023-04-18 11:29:10 +0200

    beta_break

--- src/arrays_method.ml
+++ src/arrays_method.ml
@@ -25,34 +25,12 @@ let rec tree_formula_of_formula form =
   | Forall (i, f) -> TForall (i, tree_formula_of_formula f)
   | Exists (i, f) -> TExists (i, tree_formula_of_formula f)
 
+let tree_of_formula f = Node (f, false, Nil, Nil)
+
 let rec tree_of_formula_list l =
   match l with 
   | [] -> Nil
   | a::q -> Node(tree_formula_of_formula a, false, tree_of_formula_list q,Nil)    
-
-let rec search_alpha t = 
-  match t with  
-  | [] -> failwith "No alpha term"
-  | TAnd(f1,f2)::q -> (TAnd(f1,f2),q)
-  | node::q -> let a,b = search_alpha q in (a,node::b)
-
-let rec search_delta l = 
-  match l with  
-  | [] -> failwith "No delta term"
-  | TExists(v,f)::q -> (TExists(v,f),q)
-  | node::q -> let a,b = search_delta q in (a,node::b)
-
-let rec search_beta l = 
-  match l with  
-  | [] -> failwith "No beta term"
-  | TOr(f1,f2)::q -> (TOr(f1,f2),q)
-  | node::q -> let a,b = search_beta q in (a,node::b)
-
-let rec search_gamma l = 
-  match l with  
-  | [] -> failwith "No gamma term"
-  | TForall(v,f)::q -> (TForall(v,f),q)
-  | node::q -> let a,b = search_gamma q in (a,node::b)
 
 let rec leaf_append_one tree tapp = 
   match tree with
@@ -70,3 +48,33 @@ let rec leaf_append_two tree tapp1 tapp2 =
   | Node (form,b,Nil,t) -> Node(form,b,Nil,leaf_append_two t tapp1 tapp2)
   | Node (form,b,t1,t2)-> Node(form,b,leaf_append_two t1 tapp1 tapp2,leaf_append_two t2 tapp1 tapp2)
   
+let rec search_alpha t = 
+  match t with  
+  | [] -> failwith "No alpha term"
+  | TAnd(f1,f2)::q -> (TAnd(f1,f2),q)
+  | node::q -> let a,b = search_alpha q in (a,node::b)
+
+let rec search_delta l = 
+  match l with  
+  | [] -> failwith "No delta term"
+  | TExists(v,f)::q -> (TExists(v,f),q)
+  | node::q -> let a,b = search_delta q in (a,node::b)
+
+let rec beta_break = function
+  | Nil -> (Nil, false)
+  | Node (TOr (f1, f2), false, t1, t2) ->
+      let t = Node (TOr (f1, f2), true, t1, t2) and n1 = tree_of_formula f1 and n2 = tree_of_formula f2 in
+      (leaf_append_two t n1 n2, true)
+  | Node (f, b, t1, t2) ->
+      let t1', b1 = beta_break t1 in
+      if b1 then (Node (f, b, t1', t2), true)
+      else
+        let t2', b2 = beta_break t2 in
+        (Node (f, b, t1, t2'), b2)
+
+let rec search_gamma l = 
+  match l with  
+  | [] -> failwith "No gamma term"
+  | TForall(v,f)::q -> (TForall(v,f),q)
+  | node::q -> let a,b = search_gamma q in (a,node::b)
+
